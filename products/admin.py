3d872968bf9798eef548d127cefb345d61692344
Sultan Beksultanov <sultanbek9899@gmail.com>
2021-12-30 19:50:56 +0600

    add new admin classes

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
+from products.models import Product, Category, SubCategory
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "id")
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "category","id")
+    search_fields = ("name", "category__name")
+    list_filter = ("category__name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category",
+        "subcategory",
+        "price",
+        "is_active",
+        "created",
+    )
+    search_fields = (
+        "name",
+        "price",
+        "category__name",
+        "subcategory_name"
+    )
+    list_filter = (
+        "category__name",
+        "subcategory__name",
+        "is_active",
+        "created",
+    )
+
--- products/models.py
+++ products/models.py
@@ -2,18 +2,46 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField("Название",max_length=50)
+    name = models.CharField("Название",max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
 
 
 class SubCategory(models.Model):
     name = models.CharField("Название", max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name="Категория")
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Подкатегория"
+        verbose_name_plural = "Подкатегории"
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
     name = models.CharField(verbose_name="Название", max_length=200)
     description = models.TextField(verbose_name="Описание")
     price = models.DecimalField(verbose_name="Цена",max_digits=10, decimal_places=2)
     image = models.ImageField("Фото", upload_to="products/images/")
+    is_active = models.BooleanField("Активный", default=True)
+
     created = models.DateTimeField(verbose_name="Дата создание", auto_now_add=True)
     updated = models.DateTimeField(verbose_name="Дата обновления", auto_now=True)
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.name
