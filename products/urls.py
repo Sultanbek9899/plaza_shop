c32feb73d4f947070e2b2a59a2d89631c13c10cd
Sultan Beksultanov <sultanbek9899@gmail.com>
2022-01-18 20:11:42 +0600

    productslist

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,11 @@ from products import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("products/", views.product_list, name="product_list")
+    path("products/", views.product_list, name="product_list"),
+    path("products/<str:category_slug>/", views.product_list, name="category_list"),
+    path("products/<str:category_slug>/<str:subcategory_slug>/",
+         views.product_list,
+         name="subcategory_list"
+         )
+    #http://127.0.0.1:8000/products/elektronika/televizory/
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from products.models import Category
+from products.models import Category, Product, SubCategory
 
 
 def index(request):
-    return render(request, "index.html" )
+    return render(request, "index.html")
 
 
-def product_list(request):
-    return render(request, 'product_list.html')
+def product_list(request, category_slug=None, subcategory_slug=None):
+    if subcategory_slug:
+        try:
+            subcategory=SubCategory.objects.get(slug=subcategory_slug)
+        except Subcategory.DoesNotExist:
+            print("Не найдено")
+        products = Product.objects.filter(is_active=True, subcategory=subcategory)
+    elif category_slug: # telefony-i-akss
+        try:
+            category = Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            print("Не найдено")
+        products = Product.objects.filter(is_active=True, category=category)
+    else:
+        products = Product.objects.filter(is_active=True)
+    context = {
+        "products": products,
+    }
+    return render(request, 'product_list.html', context=context)
 
