3211221826880ed76e864fce3f76fbc1d6d532e4
Sultan Beksultanov <sultanbek9899@gmail.com>
2022-02-03 18:36:59 +0600

    cart page

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path("", views.CartPageView.as_view(), name="cart_page"),
     path("add/<int:product_id>/", views.AddCartView.as_view(), name="add_cart")
 ]
--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,7 @@ from cart.cart import Cart
 from products.models import Product
 from django.http import  HttpResponseRedirect
 from django.shortcuts import  reverse
-
+from django.views.generic import  TemplateView
 
 class AddCartView(View):
     template_name = "cart.html"
@@ -16,4 +16,8 @@ class AddCartView(View):
         product = Product.objects.get(id=product_id)
         cart.add(product=product)
         return HttpResponseRedirect(reverse("index"))
+
+
+class CartPageView(TemplateView):
+    template_name = "cart.html"
 
